e52e82c1494bcb108b8223b6dde71ca84b68ee89
Tomoro0726 <s.tomoro0726@gmail.com~>
2026-05-17 15:36:22 +0900

    テストを更新

--- tests/plateau/tran.rs
+++ tests/plateau/tran.rs
@@ -7,7 +7,7 @@ use std::fs;
 fn test_tran_snapshots() {
     glob!("../../sample/plateau/tran", "**/*.gml", |path| {
         let gml = fs::read_to_string(path).unwrap();
-        let a = plateau::tran(&gml, 23, 0.0).unwrap();
+        let a = plateau::tran(&gml, 24, 0.0).unwrap();
 
         let mut results: Vec<String> = Vec::new();
 
